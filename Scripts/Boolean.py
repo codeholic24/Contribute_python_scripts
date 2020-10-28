76e34e7612ac23e7cf643198e81922113d7c83cb
Vikas B Vooradi <72571474+codeholic24@users.noreply.github.com>
2020-10-29 00:20:49 +0530

    Create Boolean.py

---
+++ Scripts/Boolean.py
@@ -0,0 +1,43 @@
+#####################################################################
+# Boolean can only be True or False
+# Note : It is compulsory to have first letter Capital
+# eg : True -> [T]rue and False -> [F]alse
+# The value of True indicate -> 1 and False indicate -> 0
+# All basic operations can be done of Boolean
+#####################################################################
+
+a=True
+print (a)
+print (type(a))
+
+#####################################################################
+
+a=False
+print (a)
+print (type(a))
+
+#####################################################################
+# True = 1 , False = 0
+
+print(True+True)    # Output :  2
+print(False+False)  # Output :  0
+
+print(True-True)    # Output :  0
+print(False-False)  # Output :  0
+
+print(True-False)   # Output :  1
+print(False-True)   # Output : -1
+
+print(True*True)    # Output :  1
+print(False*False)  # Output :  0
+
+print(True*False)   # Output :  0
+print(False*True)   # Output :  0
+
+print(False/True)   # Output :  0.0
+#print(True/False)  # ZeroDivisionError: division by zero
+
+print(True/True)    # Output : 1.0
+print(False/False)  # ZeroDivisionError: division by zero
+
+#####################################################################
