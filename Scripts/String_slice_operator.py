527c66c894c57d5f4c23847eec2c43e91dfae1d0
Vikas B Vooradi <72571474+codeholic24@users.noreply.github.com>
2020-10-29 13:25:17 +0530

    Update String_slice_operator.py

--- Scripts/String_slice_operator.py
+++ Scripts/String_slice_operator.py
@@ -29,3 +29,28 @@ print(a[4:1])
 # Output : '' -> return empty : never throw any error : IndexError: string index out of range
 
 ####################################################################################################
+# Application where it can be used
+
+# Print the first character as capital : Vikas
+s='vikas'
+print(s[0].upper()+s[1:])
+print(s[-5].upper()+s[1:])
+print(s[len(s)-len(s)].upper()+s[1:])
+
+# Output for all is : Vikas
+
+# Print the last character as capital : vikaS
+a='vikas'
+print(s[0:len(s)-1]+s[-1].upper())
+print(s[:len(s)-1]+s[4].upper())
+print(s[0:len(s)-1]+s[len(s)-1].upper())
+
+# Output for all is : vikaS
+
+# Print the first and last character as capital : VikaS
+c='vikas'
+print(s[0].upper()+s[1:len(c)-1]+s[-1].upper())
+# Output : VikaS
+
+####################################################################################################
+
