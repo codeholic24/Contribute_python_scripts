0d71daf7ecf8b959f200a0f33463342050d719a6
Vikas B Vooradi <72571474+codeholic24@users.noreply.github.com>
2020-11-24 00:41:27 +0530

    Create Important_concept.py

---
+++ Scripts/Important_concept.py
@@ -0,0 +1,26 @@
+# FLOAT :
+# floating point values can only be re-presented in decimal
+# eg : a=123.456
+
+##########################################################################
+
+# Case 1 :
+# a=0o123.234
+# print(a)
+# Output : SyntaxError: invalid syntax
+
+##########################################################################
+
+# Case 2 :
+# a=0x12.234
+# print(a)
+# Output : SyntaxError: invalid syntax
+
+##########################################################################
+
+# Case 3 :
+# a=0b1101.234
+# print(a)
+# Output : SyntaxError: invalid syntax
+
+##########################################################################
