8c3e286e8300e8a19a8229ad332dae1e86e5d9e4
Vikas B Vooradi <72571474+codeholic24@users.noreply.github.com>
2020-11-24 00:55:31 +0530

    Update Important_concept.py

--- Scripts/Important_concept.py
+++ Scripts/Important_concept.py
@@ -24,3 +24,17 @@
 # Output : SyntaxError: invalid syntax
 
 ##########################################################################
+ 
+# Case 4.1 : Exponential form is applicable only for floating point values
+# a=1.3e3
+# print(a)
+# Output : 1300.0
+
+# Case 4.2 : Exponential form is applicable only for floating point values
+# a=1.3E3
+# print(a)
+# Output : 1300.0
+
+# It as a bigger advantage when want to display bigger values 
+
+##########################################################################
